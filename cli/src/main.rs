bdbcfff31e4eb3dec6340b1a82ade114b4121b82
TheWisePigeon <zozozozeph@gmail.com>
2023-04-18 23:21:45 +0000

    feat: add arg parsing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,5 +3,21 @@ pub mod ira;
 use ira::{ parse_action };
 
 fn main() {
-    println!("Hello, world!");
+    let arg1 = std::env::args().nth(1);
+    match arg1 {
+        Some(arg1)=>{
+            match arg1.as_str() {
+                "help" =>{
+                    println!("Help for ira cli");
+                },
+                _ =>{
+
+                }
+            }
+        },
+        None=>{
+            println!("IRA CLI is a tool that helps you replicate your dev environment accross devices easily using just a config gile");
+            println!("Run `ira help` to learn more or visit ira website https://github.com/TheWisePigeon/ira");
+        }
+    }
 }
