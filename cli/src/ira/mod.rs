fb47fff20b056ffc0d00bc598ad45c230f03f99c
TheWisePigeon <zozozozeph@gmail.com>
2023-04-18 23:31:03 +0000

    feat: add main structs

--- cli/src/ira/mod.rs
+++ cli/src/ira/mod.rs
@@ -1,3 +1,30 @@
+struct Action {
+    name: String,
+    os: String,
+    requires: Vec<String>,
+    commands: Vec<String>
+}
+
+struct Step {
+    name: String,
+    uses: String,
+    args: Vec<String>,
+
+}
+
+struct ParsedConfig {
+    os_match: bool,
+    steps: Vec<Step>
+}
+
 pub fn parse_action(){
+
+}
+
+pub fn parse_config_file( path: String){
+
+}
+
+pub fn run(  ){
 
 }
