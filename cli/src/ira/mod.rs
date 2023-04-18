44aaa143f1d6343e783e7c1e42d4b93d29262b75
TheWisePigeon <zozozozeph@gmail.com>
2023-04-18 23:40:28 +0000

    feat: verify file extension before parsing

--- cli/src/ira/mod.rs
+++ cli/src/ira/mod.rs
@@ -1,28 +1,39 @@
-struct Action {
-    name: String,
-    os: String,
-    requires: Vec<String>,
-    commands: Vec<String>
+pub struct Action {
+    pub name: String,
+    pub os: String,
+    pub requires: Vec<String>,
+    pub commands: Vec<String>
 }
 
-struct Step {
-    name: String,
-    uses: String,
-    args: Vec<String>,
+pub struct Step {
+    pub name: String,
+    pub uses: String,
+    pub args: Vec<String>,
 
 }
 
-struct ParsedConfig {
-    os_match: bool,
-    steps: Vec<Step>
+pub struct ParsedConfig {
+    pub os_match: bool,
+    pub steps: Vec<Step>
+}
+
+pub struct ParsingError {
+    pub message: String
 }
 
 pub fn parse_action(){
 
 }
 
-pub fn parse_config_file( path: String){
 
+
+pub fn parse_config_file( path: String)-> Result<ParsedConfig, ParsingError>  {
+    let path_is_valid = path.ends_with(".ira.conf");
+    if ! path_is_valid {
+        return Err(ParsingError { message: String::from("File is not a valid ira config file") })
+    }else {
+        Ok(ParsedConfig { os_match: true, steps: vec![] })
+    }
 }
 
 pub fn run(  ){
